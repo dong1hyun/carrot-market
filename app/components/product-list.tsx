8aeece98985660d7575aa6211e42e7d19af4c7d8
dong1hyun <limd1238@naver.com>
2024-07-09 13:21:05 +0900

    test

--- app/components/product-list.tsx
+++ app/components/product-list.tsx
@@ -10,40 +10,40 @@ interface ProductList {
 }
 
 export default function ProductList({ initialProducts }: ProductList) {
-    const [products, setProducts] = useState(initialProducts);
+    const [products, setProducts] = useState([{id:1, title:"yeah", price:999, photo:"yeah", created_at: new Date()}]);
     const [isLoading, setIsLoading] = useState(false);
     const [page, setPage] = useState(0);
     const [isLastPage, setIsLastPage] = useState(false);
     const trigger = useRef<HTMLSpanElement>(null);
     useEffect(() => {
-        const observer = new IntersectionObserver(
-            async (entries: IntersectionObserverEntry[],
-                observer: IntersectionObserver) => {
-                const element = entries[0];
-                if (element.isIntersecting && trigger.current) {
-                    observer.unobserve(trigger.current);
-                    setIsLoading(true);
-                    const newProducts = await getMoreProducts(page + 1);
-                    if (newProducts.length !== 0) {
-                        setPage(prev => prev + 1); // 받은 상품 배열이 0개가 아닐 때만 다음 페이지로 넘어감 
-                        setProducts(cur => [...cur, ...newProducts]);
-                    } else {
-                        setIsLastPage(true);
-                    }
-                    setIsLoading(false);
-                }
-            },
-            {
-                threshold: 1.0,
-                rootMargin: "0px 0px -200px 0px" //observer 범위 설정
-            }
-        );
-        if (trigger.current) {
-            observer.observe(trigger.current);
-        }
-        return () => {
-            observer.disconnect();
-        }
+        // const observer = new IntersectionObserver(
+        //     async (entries: IntersectionObserverEntry[],
+        //         observer: IntersectionObserver) => {
+        //         const element = entries[0];
+        //         if (element.isIntersecting && trigger.current) {
+        //             observer.unobserve(trigger.current);
+        //             setIsLoading(true);
+        //             const newProducts = await getMoreProducts(page + 1);
+        //             if (newProducts.length !== 0) {
+        //                 setPage(prev => prev + 1); // 받은 상품 배열이 0개가 아닐 때만 다음 페이지로 넘어감 
+        //                 setProducts(cur => [...cur, ...newProducts]);
+        //             } else {
+        //                 setIsLastPage(true);
+        //             }
+        //             setIsLoading(false);
+        //         }
+        //     },
+        //     {
+        //         threshold: 1.0,
+        //         rootMargin: "0px 0px -200px 0px" //observer 범위 설정
+        //     }
+        // );
+        // if (trigger.current) {
+        //     observer.observe(trigger.current);
+        // }
+        // return () => {
+        //     observer.disconnect();
+        // }
     }, [page]);
     return (
         <div className="p-5 flex flex-col gap-5">
